0353b43b83d7bbb0b25fee2054483372e9a5dd58
HRahimy <rahimy.hamza@gmail.com>
2021-06-20 23:57:58 +0300

    Implement site layout

--- libs/site-ui/layout/src/lib/site-ui-layout.module.ts
+++ libs/site-ui/layout/src/lib/site-ui-layout.module.ts
@@ -2,11 +2,17 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AppScaffoldComponent} from './app-scaffold/app-scaffold.component';
 import {SiteUiSharedUiModule} from "@hamza/site-ui/shared/ui";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatLineModule} from "@angular/material/core";
 
 @NgModule({
   imports: [
     CommonModule,
-    SiteUiSharedUiModule
+    SiteUiSharedUiModule,
+    MatTabsModule,
+    MatDividerModule,
+    MatLineModule
   ],
   declarations: [
     AppScaffoldComponent
--- libs/site-ui/shared/ui/src/lib/site-ui-shared-ui.module.ts
+++ libs/site-ui/shared/ui/src/lib/site-ui-shared-ui.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SummaryComponent } from './summary/summary.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SummaryComponent} from './summary/summary.component';
+import {MatLineModule} from "@angular/material/core";
+import {FooterComponent} from './footer/footer.component';
+import {MatDividerModule} from "@angular/material/divider";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatLineModule,
+    MatDividerModule,
+    MatToolbarModule
+  ],
   declarations: [
-    SummaryComponent
+    SummaryComponent,
+    FooterComponent
   ],
   exports: [
-    SummaryComponent
+    SummaryComponent,
+    FooterComponent
   ]
 })
-export class SiteUiSharedUiModule {}
+export class SiteUiSharedUiModule {
+}
