2e59f8f3d118c5c85d930461bf4f2a08336e5904
HRahimy <rahimy.hamza@gmail.com>
2021-06-23 17:44:47 +0300

    Fix avatar position

--- libs/site-ui/content/src/lib/site-ui-content.module.ts
+++ libs/site-ui/content/src/lib/site-ui-content.module.ts
@@ -3,12 +3,14 @@ import {CommonModule} from '@angular/common';
 import {SkillsComponent} from './skills/skills.component';
 import {MatListModule} from "@angular/material/list";
 import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
 
 @NgModule({
   imports: [
     CommonModule,
     MatListModule,
     MatIconModule,
+    MatDividerModule
   ],
   declarations: [
     SkillsComponent
